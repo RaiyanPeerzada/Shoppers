85adb61308323b15d15c0810bedfb58ec58bd37d
RaiyanPeerzada <p_raiyan@outlook.com>
2019-11-12 23:24:05 -0600

    Added about and contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'contact/index'
+  get 'about/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'products/index'
   get 'categories/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'categories#index'
+
+  # root 'about#index'
 
   resources :categories, only: [:index] do
     resources :products, only: [:index]
