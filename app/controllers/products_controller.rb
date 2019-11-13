514cc43641e64fe734a81dd0a00e95e86662b70f
RaiyanPeerzada <p_raiyan@outlook.com>
2019-11-12 21:40:55 -0600

    product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+    @products = @category.products.includes(:variants).order(:title)
+  end
+end
